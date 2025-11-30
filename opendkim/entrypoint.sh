37deaca51a705736c654f6f78db1d12b016f6678
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-11-30 02:41:11 +0200

    refactor(opendkim): add extension points

--- opendkim/entrypoint.sh
+++ opendkim/entrypoint.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+set -e
+
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 /usr/sbin/syslogd -n -O - -S &
 pid1=$!
 
-/usr/sbin/opendkim -u opendkim -p inet:8891@0.0.0.0 -P /run/opendkim/opendkim.pid -f &
+cp -f /etc/opendkim/opendkim.conf.base /etc/opendkim.conf
+for file in /etc/opendkim/conf.d/*.conf; do
+    if [ -f "${file}" ]; then
+        printf "\n\n#\n# %s\n#\n" "${file}" >> /etc/opendkim.conf
+        cat "${file}" >> /etc/opendkim.conf
+    fi
+done
+
+/usr/sbin/opendkim -x /etc/opendkim.conf -f &
 pid2=$!
 
+set +e
 # shellcheck disable=SC2064
 trap "kill ${pid1} ${pid2}" INT TERM
 wait
